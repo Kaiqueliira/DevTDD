39d0e7becd4eae7b0c84ad3774ef93105f7dd125
Kaique lira <Kaiqueliira@gmail.com>
2024-10-08 16:31:56 -0300

    Implement logic to return false for years divisible by 100

--- src/DevTDD.Core/Services/LeapYearServices.cs
+++ src/DevTDD.Core/Services/LeapYearServices.cs
@@ -5,7 +5,7 @@ public class LeapYearServices
 {
     public static bool IsLeapYear(int year)
     {
-        if (year % 4 == 0) return true;
+        if (year % 4 == 0 && year % 100 != 0) return true;
 
         return false;
     }
