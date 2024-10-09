96214733de35b0c0ea6ceaf4c99070d1dde1c94c
Kaique lira <Kaiqueliira@gmail.com>
2024-10-08 23:11:27 -0300

    Fibonacci should return 0 for position 0

---
+++ src/DevTDD.Core/Services/FibonacciServices.cs
@@ -0,0 +1,11 @@
+﻿
+namespace DevTDD.Core.Services
+{
+    public class FibonacciServices
+    {
+        public static int GetFibonacci(int position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/DevTDD.Tests/Module1/FibonacciTests.cs
@@ -0,0 +1,20 @@
+﻿using DevTDD.Core.Services;
+using FluentAssertions;
+
+namespace DevTDD.Tests.Module1
+{
+    public class FibonacciTests
+    {
+        [Fact]
+        public void Fibonacci_PositionZero_ShouldReturnZero()
+        {
+            int position = 0;
+
+            int result = FibonacciServices.GetFibonacci(position);
+
+            result.Should().Be(0);
+        }
+    }
+}
+
+
