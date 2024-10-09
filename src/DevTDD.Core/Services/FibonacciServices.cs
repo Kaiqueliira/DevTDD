9c5613e6e9c216f114601b15be30d6777ffd093b
Kaique lira <Kaiqueliira@gmail.com>
2024-10-08 23:37:56 -0300

    Implement GetFibonacci function using with tuple deconstruction

--- src/DevTDD.Core/Services/FibonacciServices.cs
+++ src/DevTDD.Core/Services/FibonacciServices.cs
@@ -6,16 +6,13 @@ namespace DevTDD.Core.Services
     {
         public static int GetFibonacci(int position)
         {
-            if (position == 0) return 0;
-            if (position == 1) return 1;
+            if (position < 2) return position;
 
             int previous = 0, current = 1;
 
             for (int i = 2; i <= position; i++)
             {
-                int next = previous + current;
-                previous = current;
-                current = next;
+                (previous, current) = (current, previous + current);
             }
 
             return current;
