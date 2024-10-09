bedf7b2ddd8128819ac371a591844c723beaddd8
Kaique lira <Kaiqueliira@gmail.com>
2024-10-08 23:22:08 -0300

    Implement Fibonacci handles positions 2 and higher

--- src/DevTDD.Core/Services/FibonacciServices.cs
+++ src/DevTDD.Core/Services/FibonacciServices.cs
@@ -9,7 +9,16 @@ namespace DevTDD.Core.Services
             if (position == 0) return 0;
             if (position == 1) return 1;
 
-            return -1;
+            int previous = 0, current = 1;
+
+            for (int i = 2; i <= position; i++)
+            {
+                int next = previous + current;
+                previous = current;
+                current = next;
+            }
+
+            return current;
         }
     }
 }
