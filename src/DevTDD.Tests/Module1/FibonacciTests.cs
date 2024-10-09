443e391d7bd080c7ad202900c7c9add1efe08173
Kaique lira <Kaiqueliira@gmail.com>
2024-10-08 23:15:47 -0300

    Test Fibonacci should return 1 for position 1

--- src/DevTDD.Tests/Module1/FibonacciTests.cs
+++ src/DevTDD.Tests/Module1/FibonacciTests.cs
@@ -6,13 +6,23 @@ namespace DevTDD.Tests.Module1
     public class FibonacciTests
     {
         [Fact]
-        public void Fibonacci_PositionZero_ShouldReturnZero()
+        public void GetFibonacci_PositionZero_ShouldReturnZero()
         {
             int position = 0;
 
             int result = FibonacciServices.GetFibonacci(position);
 
             result.Should().Be(0);
+        }
+
+        [Fact]
+        public void GetFibonacci_PositionOne_ShouldReturnOne()
+        {
+            int position = 1;
+
+            int result = FibonacciServices.GetFibonacci(position);
+
+            result.Should().Be(1);
         }
     }
 }
