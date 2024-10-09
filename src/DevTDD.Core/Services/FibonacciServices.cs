8e6c13843acfa1ef8e7e2a686338dc9ca7de633a
Kaique lira <Kaiqueliira@gmail.com>
2024-10-08 23:16:32 -0300

    Implement Fibonacci returns 1 for position 1

--- src/DevTDD.Core/Services/FibonacciServices.cs
+++ src/DevTDD.Core/Services/FibonacciServices.cs
@@ -7,6 +7,7 @@ namespace DevTDD.Core.Services
         public static int GetFibonacci(int position)
         {
             if (position == 0) return 0;
+            if (position == 1) return 1;
 
             return -1;
         }
